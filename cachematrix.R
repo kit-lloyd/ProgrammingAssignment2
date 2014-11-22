5d18e1d3ffebe37984e46c281943cdb48a27aff1
kit-lloyd <kitlloyd@gmail.com>
2014-11-22 19:43:59 +0400

    Updated comments
    Updated comments to impart more clarity

--- cachematrix.R
+++ cachematrix.R
@@ -7,20 +7,31 @@
 ## makeCacheMatrix accepts the matrix and establishes the
 ## set, get, setinverse and getinverse parameters
 makeCacheMatrix <- function(x = matrix()) {
-## set the value of the matrix
+
+## k will be the inverse, set to NULL by makeCacheMatrix
     k <- NULL
     set <- function(y) {
         x <<- y
         k <<- NULL
     }
-## get the value of the matrix
+    
+## note these next three functions are defined but not run when makeCacheMatrix is called.
+## instead, they will be used by cachesolve() to get values for x or for
+## k (solve) and for setting the inverse.  These are usually called object 'methods'
+
+## this function returns the value of the original vector
     get <- function() x
 
-## set the inverse of the matrix
+## this is called by cacheSolve() during the first cacheSolve()
+##  access and it will store the value using superassignment
     setinverse <- function(solve) k <<- solve
+    
+## this will return the cached value to cacheSolve() on subsequent accesses
     getinverse <- function() k
 
-## get the inverse of the matrix
+## This is accessed each time makeCacheMatrix() is called
+## This is a list of the internal functions ('methods') so a calling function
+## knows how to access those methods.
     list(set = set, get = get,
          setinverse = setinverse,
          getinverse = getinverse)
@@ -31,20 +42,26 @@ makeCacheMatrix <- function(x = matrix()) {
 ## unless the inverse has already been calculated, in which case it
 ## retrieves it from the cache.
 
+## the input x is an object created by makeCacheMatrix
+
 cacheSolve <- function(x, ...) {
   
-## get the inverse of the matrix        
+## accesses the object 'x' and gets the value of the inverse        
     k <- x$getinverse()
   
-## check if there is the matrix in k    
+## check if there is the matrix in k (ie already cached)
+## and send a message to console if it is
     if(!is.null(k)) {
         message("getting cached data")
         return(k)
     }
-## if not: get the inverse of the matrix and load into k   
+## we reach this code only if x$getinverse() returned NULL
+## get the inverse of the matrix and load into k   
     data <- x$get()
     k <- solve(data, ...)
-## set the inverse of the matrix (uses the solve function)
+## store the calculated inverse value in x 
+## (see setinverse() in makeCacheMatrix
     x$setinverse(k)
+## return the inverse to the code that called this function
     k
 }
